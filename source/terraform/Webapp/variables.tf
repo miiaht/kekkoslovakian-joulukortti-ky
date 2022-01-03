c8e4493b2ae2c8ab364ed4aa8de2e24a8abb33dc
Ville <koivisto.vt@gmail.com>
2022-01-03 12:05:49 +0200

    web-sovelluksen terraform-templatet

---
+++ source/terraform/Webapp/variables.tf
@@ -0,0 +1,12 @@
+variable "project" {}
+
+variable "credentials_file" {}
+
+# Default location placeholders:
+variable "region" {
+    default = "us-central1"
+}
+
+variable "zone" {
+    default = "us-central1-c"
+}
