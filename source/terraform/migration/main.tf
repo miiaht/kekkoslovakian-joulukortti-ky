1b685957425d2004d832e9075162eb86652a3100
jphieno <jari-pekka.heino@outlook.com>
2022-01-05 16:49:27 +0200

    uncommented sql codes

--- source/terraform/migration/main.tf
+++ source/terraform/migration/main.tf
@@ -228,77 +228,77 @@ resource "google_iap_tunnel_instance_iam_binding" "tunnel_user_iam_res" {
   members  = var.members
 }
 
-# ### Private IP -säännöt SQL:lle
+### Private IP -säännöt SQL:lle
 
-# resource "google_compute_global_address" "private_ip_address" {
-#   provider      = google-beta
-#   name          = "private-ip-address"
-#   purpose       = "VPC_PEERING"
-#   address_type  = "INTERNAL"
-#   prefix_length = 16
-#   network       = google_compute_network.vpc_network.id
-# }
+resource "google_compute_global_address" "private_ip_address" {
+  provider      = google-beta
+  name          = "private-ip-address"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = google_compute_network.vpc_network.id
+}
 
-# resource "google_service_networking_connection" "private_vpc_connection" {
-#   provider                = google-beta
-#   network                 = google_compute_network.vpc_network.id
-#   service                 = "servicenetworking.googleapis.com"
-#   reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
-# } 
+resource "google_service_networking_connection" "private_vpc_connection" {
+  provider                = google-beta
+  network                 = google_compute_network.vpc_network.id
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
+} 
 
-# #######################################
-# #   SQL-tietokanta Kekkoslovakialle   #
-# #######################################
+#######################################
+#   SQL-tietokanta Kekkoslovakialle   #
+#######################################
 
-# resource "google_sql_database_instance" "kekkoslovakia_sql_instanssi" {
-#   provider         = google-beta
-#   name             = "kekkoslovakia"
-#   database_version = "POSTGRES_13"
-#   region           = var.region
-
-
-#   depends_on = [google_service_networking_connection.private_vpc_connection]
-
-#   settings {
-#     tier = "db-f1-micro"
-#     disk_size = 10
-
-#     ip_configuration {
-#       ipv4_enabled    = false
-#       private_network = google_compute_network.vpc_network.id
-#     }
-#   }
-# }
-
-# ###########################################
-# #   Henkiöstö-database SQL-tietokantaan   #
-# ###########################################
-
-# resource "google_sql_database" "henkilosto_database" {
-#   name       = "henkilosto"
-#   instance   = google_sql_database_instance.kekkoslovakia_sql_instanssi.id
-# }
-
-# resource "google_sql_user" "henkilosto_database_user" {
-#   name       = var.henkilosto_database_username
-#   instance   = google_sql_database_instance.kekkoslovakia_sql_instanssi.id
-#   password   = var.henkilosto_database_password
-# }
+resource "google_sql_database_instance" "kekkoslovakia_sql_instanssi" {
+  provider         = google-beta
+  name             = "kekkoslovakia"
+  database_version = "POSTGRES_13"
+  region           = var.region
 
 
-# ###########################################
-# #   Reskontra-database SQL-tietokantaan   #
-# ###########################################
+  depends_on = [google_service_networking_connection.private_vpc_connection]
+
+  settings {
+    tier = "db-f1-micro"
+    disk_size = 10
+
+    ip_configuration {
+      ipv4_enabled    = false
+      private_network = google_compute_network.vpc_network.id
+    }
+  }
+}
+
+###########################################
+#   Henkiöstö-database SQL-tietokantaan   #
+###########################################
+
+resource "google_sql_database" "henkilosto_database" {
+  name       = "henkilosto"
+  instance   = google_sql_database_instance.kekkoslovakia_sql_instanssi.id
+}
+
+resource "google_sql_user" "henkilosto_database_user" {
+  name       = var.henkilosto_database_username
+  instance   = google_sql_database_instance.kekkoslovakia_sql_instanssi.id
+  password   = var.henkilosto_database_password
+}
 
 
-# resource "google_sql_database" "reskontra_database" {
-#   name     = "reskontra"
-#   instance = google_sql_database_instance.kekkoslovakia_sql_instanssi.id
-# }
+###########################################
+#   Reskontra-database SQL-tietokantaan   #
+###########################################
 
-# resource "google_sql_user" "reskontra_database_user" {
-#   name     = var.reskontra_database_username
-#   instance = google_sql_database_instance.kekkoslovakia_sql_instanssi.id
-#   password = var.reskontra_database_password
-# }
+
+resource "google_sql_database" "reskontra_database" {
+  name     = "reskontra"
+  instance = google_sql_database_instance.kekkoslovakia_sql_instanssi.id
+}
+
+resource "google_sql_user" "reskontra_database_user" {
+  name     = var.reskontra_database_username
+  instance = google_sql_database_instance.kekkoslovakia_sql_instanssi.id
+  password = var.reskontra_database_password
+}
 
