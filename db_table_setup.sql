ae7d75bf11edf11d2940d9fee4374c3393049391
miiaht <mhtakaw2021@gmail.com>
2022-01-06 21:21:46 +0200

    modified sql table column to autopopulate hasbeenread to false

--- db_table_setup.sql
+++ db_table_setup.sql
@@ -3,7 +3,7 @@ CREATE TABLE kortit (
     lahettaja varchar(50) NOT NULL,
     tervehdysteksti varchar(250) NOT NULL,
     vastaanottajanemail varchar(50) NOT NULL,
-    hasbeenread BOOLEAN NOT NULL,
+    hasbeenread BOOLEAN NOT NULL DEFAULT FALSE,
     dateCreated TIMESTAMP NOT NULL DEFAULT(NOW()),
     kuvaURL VARCHAR(250) NOT NULL
 );
