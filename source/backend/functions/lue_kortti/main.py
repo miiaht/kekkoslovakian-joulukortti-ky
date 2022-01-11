20fdcd45c9c517cb1d43ff0de6f447a0a7e9d1e6
hanezki <hanezki.manninen@gmail.com>
2022-01-11 17:18:40 +0200

    frontin formi saatu melko valmiiksi

--- source/backend/functions/lue_kortti/main.py
+++ source/backend/functions/lue_kortti/main.py
@@ -97,6 +97,25 @@ def html_kortti(lahettaja, teksti, kuvan_url):
 
     return kortti
 
+### Elina kommentoi alkuperäisen funktion
+# def html_kortti(lahettaja, teksti, kuvan_url):
+#     kortti = f'<!doctype html>\
+#     <html>\
+#         <head>\
+#             <title>Hyvää joulua!</title>\
+#         </head>\
+#         <body style="background-color:#f7f4eb;">\
+#             <h1>{teksti}</h1>\
+#             <p>\
+#                 <img src="{kuvan_url}" alt="christmas_image" style="max-width:100%;height:auto;">\
+#             </p>\
+#             <h2>{lahettaja}</h2>\
+#                 <h5>Kekkoslovakian Joulukortit Ky</h5>\
+#         </body>\
+#     </html>'
+
+#     return kortti
+
 
 def hae_kirjautumistiedot(project_id):
     client = secretmanager.SecretManagerServiceClient()
--- source/frontend/app.py
+++ source/frontend/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, widgets
+from wtforms import StringField, SubmitField, widgets, BooleanField
 from wtforms.fields.choices import RadioField, SelectField
 from wtforms.validators import DataRequired, Email
 from dotenv import load_dotenv
@@ -10,17 +10,14 @@ import requests
 import json
 
 
-images = "https://storage.cloud.google.com/kekkos-ampari123/Asset%201.png?authuser=0", "https://storage.cloud.google.com/kekkos-ampari123/Asset%202.png?authuser=0"
+
 #Formi jota käytetään index.html:ssä
 class LetterForm(FlaskForm):
+    image = RadioField('style image.', choices=[('kortti1_blank.png', 'Asset1' ),('kortti2_blank.png','Asset3'),('kortti3_blank.png','Asset3')] )
     sender = StringField('Lähettäjän nimi', validators=[DataRequired()])
     message = StringField('Kirjoita tähän joleterkut', validators=[DataRequired()])
     receiver = StringField('Vastaanottajan sähköposti', validators=[DataRequired()])
-    image = RadioField(validators=[DataRequired()], choices=images)
     submit = SubmitField('Lähetä')
-
-
-
 
 app = Flask(__name__)
 
@@ -37,16 +34,30 @@ def index():
     # you must tell the variable 'form' what you named the class, above
     # 'form' is the variable name used in this template: index.html
     form = LetterForm()
+    print(form.errors)
+
+    # if form.is_submitted():
+    #     print ("submitted")
+
+    # if form.validate():
+    #     print("valid")
+
+    print(form.errors)
     if form.validate_on_submit():
-        post_data(form.data)
+        print("Yritetään postittaa")
+        post_data(form.data)    
         return redirect(url_for("index"))
+    # else:
+    #     print("validointi meni vituiks")
     
     return render_template('index.html', form=form)
 
 #Lähetetään formin datan post metodilla json muodossa apiin.
 def post_data(data_to_post):
-    json_object = json.dumps(data_to_post, indent = 4) 
+    json_object = json.dumps(data_to_post, indent = 5) 
+    print(json_object)
     url = os.environ.get('postUrl')
     headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    
     x = requests.post(url, data = json_object, headers=headers)
     print(x.text)
