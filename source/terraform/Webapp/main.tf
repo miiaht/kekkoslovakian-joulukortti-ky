4f62c1985eae785858602ec5c976e60411bed17b
Ville <koivisto.vt@gmail.com>
2022-01-12 16:08:53 +0200

    backendin funktioiden kansiorakenteen järkkäilyä

--- source/terraform/Webapp/main.tf
+++ source/terraform/Webapp/main.tf
@@ -287,3 +287,119 @@ resource "google_api_gateway_gateway" "kekkos-gw" {
   api_config = google_api_gateway_api_config.kekkos-gw.id
   gateway_id = "kekkos-gw"
 }
+
+
+
+/***********************************************************
+Cloud Functions -funktiot
+***********************************************************/
+
+### Luodaan ämpäri, zipit ja funktiot
+# Access control bucketille
+resource "google_storage_bucket_access_control" "public_rule" {
+  bucket = google_storage_bucket.bucket.name
+  role   = "OWNER"
+  entity = "allUsers"
+}
+
+# Ämpäri jossa koodit funktioille
+resource "google_storage_bucket" "bucket" {
+  provider = google
+  name     = "kekkos-koodit"
+  location = "US"
+}
+
+# Zipattu koodi #1 ämpäriin
+resource "google_storage_bucket_object" "zip_1" {
+  provider  = google
+  name      = "get_all_items"
+  bucket    = google_storage_bucket.bucket.name
+  source    = "../functions/get_all_items.zip"
+}
+
+# Luo funktio zipissä olevasta koodi #1 stä
+resource "google_cloudfunctions_function" "func_1" {
+  provider    = google
+  name        = "get_all_items"
+  description = "Hakee kaikki tuotteet tietokannasta"
+  runtime     = "python39"
+
+  available_memory_mb   = 128
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.zip_1.name
+  trigger_http          = true
+  entry_point           = "get_all_items"
+}
+
+# Zipattu koodi #2 ämpäriin
+resource "google_storage_bucket_object" "zip_2" {
+  provider  = google
+  name      = "insert_to_cart"
+  bucket    = google_storage_bucket.bucket.name
+  source    = "../functions/push_to_cart.zip"
+}
+
+# Luo funktio zipissä olevasta koodi #2 stä
+resource "google_cloudfunctions_function" "func_2" {
+  provider    = google
+  name        = "insert_to_cart"
+  description = "Hakee kaikki tuotteet tietokannasta"
+  runtime     = "python39"
+
+  available_memory_mb   = 128
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.zip_2.name
+  trigger_http          = true
+  entry_point           = "insert_to_cart"
+}
+
+# Zipattu koodi #3 ämpäriin
+resource "google_storage_bucket_object" "zip_3" {
+  provider  = google
+  name      = "get_cart"
+  bucket    = google_storage_bucket.bucket.name
+  source    = "../functions/get_cart.zip"
+}
+
+# Luo funktio zipissä olevasta koodi #3 stä
+resource "google_cloudfunctions_function" "func_3" {
+  provider    = google
+  name        = "get_cart"
+  description = "Hakee kaikki tuotteet tietokannasta"
+  runtime     = "python39"
+
+  available_memory_mb   = 128
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.zip_3.name
+  trigger_http          = true
+  entry_point           = "get_cart"
+}
+
+# avataan pääsy funktioihin
+resource "google_cloudfunctions_function_iam_member" "invoker_1" {
+  provider       = google
+  cloud_function = google_cloudfunctions_function.func_1.name
+  role   = "roles/cloudfunctions.invoker"
+  member = "allUsers"
+}
+
+resource "google_cloudfunctions_function_iam_member" "invoker_2" {
+  provider       = google
+  cloud_function = google_cloudfunctions_function.func_2.name
+  role   = "roles/cloudfunctions.invoker"
+  member = "allUsers"
+}
+
+resource "google_cloudfunctions_function_iam_member" "invoker_3" {
+  provider       = google
+  cloud_function = google_cloudfunctions_function.func_3.name
+  role   = "roles/cloudfunctions.invoker"
+  member = "allUsers"
+}
+
+resource "google_cloudfunctions_function_iam_member" "invoker_4" {
+  provider       = google
+  cloud_function = google_cloudfunctions_function.func_4.name
+  role   = "roles/cloudfunctions.invoker"
+  member = "allUsers"
+}
